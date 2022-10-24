0b1a6de321de4c68fd09e6eee48fd0a433a3cb72
Dennis Chiu <dennis_chiu@cathaypacific.com>
2022-10-24 21:34:49 +0800

    remove unused code

--- WarmUpKitMobile/WarmUpKitMobile/HomePage/HomePageViewController.swift
+++ WarmUpKitMobile/WarmUpKitMobile/HomePage/HomePageViewController.swift
@@ -47,7 +47,7 @@ class HomePageViewController: UIViewController {
         healthKitManager.authorizeHealthKitAccess { (success, error) in
             self.healthKitManager.getActiveEnergy()
         }
-//        getHealthKitStepsInfo()
+        
         healthKitManager.getActiveEnergy()
         healthKitManager.getHealthKitStepsInfo()
     }
@@ -74,7 +74,6 @@ class HomePageViewController: UIViewController {
         barChart.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-//        getHealthKitStepsInfo()
     }
     
     @IBAction func buttonAction(_ sender: Any) {
@@ -86,116 +85,6 @@ class HomePageViewController: UIViewController {
             self.present(viewController, animated: true) {}
         }
     }
-}
-
-extension HomePageViewController {
-//    func getHealthKitStepsInfo() {
-//        guard HKHealthStore.isHealthDataAvailable() else {
-//            return
-//        }
-//
-//        let startOfMonth = healthKitManager.getStartDay()
-//        let endOfMonth = healthKitManager.getEndDay(startOfMonth: startOfMonth)
-//        let dayInt = healthKitManager.getDayInt(endOfMonth: endOfMonth)
-//        print("startOfMonth", startOfMonth)
-//        print("endOfMonth", endOfMonth)
-//        print("dayInt", dayInt)
-//
-//        guard let stepCountType = HKObjectType.quantityType(forIdentifier: .stepCount) else {
-//            fatalError("*** Unable to get the step count type ***")
-//        }
-//
-//        var interval = DateComponents()
-//        interval.day = 1
-//
-//        let calendar = Calendar.current
-//        //        let anchorDate = calendar.date(bySettingHour: 12, minute: 0, second: 0, of: self.date)
-//        let anchorDate = calendar.date(bySetting: .day, value: 1, of: healthKitManager.date)
-//
-//        let query = HKStatisticsCollectionQuery.init(quantityType: stepCountType,
-//                                                     quantitySamplePredicate: nil,
-//                                                     options: .cumulativeSum,
-//                                                     anchorDate: anchorDate!,
-//                                                     intervalComponents: interval)
-//
-//        query.initialResultsHandler = {
-//            query, results, error in
-//
-//            guard let statsCollection = results else {
-//                // Perform proper error handling here
-//                return
-//            }
-//            var dataEntries = [BarChartDataEntry]()
-//
-//            var thisDay = startOfMonth
-//            var totalSteps = 0.0
-//            for x in 1...10 {
-//                print(x)
-//                let barEntry = BarChartDataEntry(x: (Double(x)), y: 5)
-//                dataEntries.append(barEntry)
-//
-//                let nextDay: Date = calendar.date(byAdding: .day, value: 1, to: thisDay)!
-//                // Plot the weekly step counts over the past 3 months
-//                statsCollection.enumerateStatistics(from: thisDay, to: thisDay) { statistics, stop in
-//
-//                    if let quantity = statistics.sumQuantity() {
-//                        let _ = statistics.startDate
-//                        let value = quantity.doubleValue(for: HKUnit.count())
-//
-////                        let barEntry = BarChartDataEntry(x: (Double(i)), y: value)
-////                        dataEntries.append(barEntry)
-//                        totalSteps += value
-//                        // Call a custom method to plot each data point.String(describing: )
-//
-//                    }
-//                }
-//                thisDay = nextDay
-//            }
-//
-//
-//            DispatchQueue.main.async {
-//                self.summaryBoard.importData(iconKey: "walk_icon", title: "Steps", firstTitle: "Steps", firstContent: String(totalSteps), secondTitle: "Average Steps", secondContent: String(totalSteps / Double(dayInt)))
-//            }
-//            print(dataEntries.count)
-//            let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Steps")
-//            chartDataSet.colors = ChartColorTemplates.colorful()
-////            chartDataSet.drawValuesEnabled = false
-//            chartDataSet.notifyDataSetChanged()
-//            let chartData = BarChartData(dataSet: chartDataSet)
-//            chartData.barWidth = 0.5
-////            chartData.notifyDataChanged()
-//            self.barChart.data = chartData
-//            self.barChart.notifyDataSetChanged()
-////            self.barChart.xAxis.labelPosition = .bottom
-//            self.barChart.animate(xAxisDuration: 1.0, yAxisDuration: 1.0)
-//        }
-//        healthKitManager.healthStore.execute(query)
-//    }
-    
-//    func getHealthEnergyInfo() {
-//        guard let energyType = HKSampleType.quantityType(forIdentifier: .activeEnergyBurned) else {
-//            print("Energy Burned type not available")
-//            return
-//        }
-//
-//        let startOfMonth = healthKitManager.getStartDay()
-//        let endOfMonth = healthKitManager.getEndDay(startOfMonth: startOfMonth)
-//        let predicate = HKQuery.predicateForSamples(withStart: startOfMonth, end: endOfMonth, options: .strictStartDate)
-//
-//        let energyQuery = HKSampleQuery(sampleType: energyType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) {(query, sample, error) in
-//            guard error == nil,let quantitySamples = sample as? [HKQuantitySample] else {
-//                print("Something went wrong: \(String(describing: error))")
-//                return
-//            }
-//
-//            let total = quantitySamples.reduce(0.0) { $0 + $1.quantity.doubleValue(for: HKUnit.kilocalorie()) }
-//
-//            DispatchQueue.main.async {
-//                self.secondSummaryBoard.importData(iconKey: "energy_icon", title: "Active Energy", firstTitle: "Active Energy", firstContent: String(total), secondTitle: "Average Kilocalories", secondContent: String(format: "%.2f", total))
-//            }
-//        }
-//        HKHealthStore().execute(energyQuery)
-//    }
 }
 
 extension HomePageViewController: WorkoutTrackingDelegate {
